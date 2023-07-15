b58a99344c7b125cfeac8846722cdb7580b8fffe
m2030 <mohamed.abouhasem@hotmail.com>
2023-07-15 20:06:32 +0300

    Added POST,PUT,PATCH and DELETE viewset

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path,include 
-from profile_api import views 
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from profile_api import views
+
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 
 urlpatterns = [
-    path('hello-view/',views.HelloApiView.as_view())
+    path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls)),
 ]
