8d4e2bb7bc5b50face766d44a0592bdc4217b8e6
m2030 <mohamed.abouhasem@hotmail.com>
2023-07-16 14:42:15 +0300

    Added user profile feed API

--- profile_api/models.py
+++ profile_api/models.py
@@ -2,7 +2,23 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
+from django.conf import settings
 
+class ProfileFeedItem(models.Model):
+    """Profile Status ipdate"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
+    
+    
+    
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
